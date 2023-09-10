f523c8014fda800741a7ea084efff042c256ac42
FrancoMariani1 <franco-mariani@hotmail.com>
2023-09-10 12:37:26 -0300

    Commit inicial

---
+++ src/test/java/com/management/sales/Sales/SalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.sales.Sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
